e5cf0a96e9683acb5733c60480dfa7002b61c9e6
Prince Kumar <princesd991@gmail.com>
2024-12-23 16:21:47 +0530

    add image upload 🍁

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
+const multer = require("multer");
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const suffix = Date.now();
+    cb(null, suffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/uploadSingleImage", upload.single("avatar"), (req, res) => {
+  res.send("Single file uploaded successfully");
+});
 
 router.post("/register", register);
 router.post("/login", login);
