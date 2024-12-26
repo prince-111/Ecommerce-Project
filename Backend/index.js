a0a18f8aab71eb246172c9e676eec099d1b78a57
Prince Kumar <princesd991@gmail.com>
2024-12-26 17:09:00 +0530

    update index with all routes 🎡

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 require("dotenv").config();
 const authRoutes = require("./src/routes/authRoutes");
+const imageRoutes = require("./src/routes/imageRoutes");
+const productRoutes = require("./src/routes/productRoutes");
+const categoryRoutes = require("./src/routes/categoryRoutes");
+const cors = require("cors");
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
+app.use(cors());
 
-app.use("/users", authRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message,
+  });
+});
+
+app.use("/api/v1/users", authRoutes);
+app.use("/api/v1/images", imageRoutes);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
