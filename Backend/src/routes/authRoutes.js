2c4743d2134a496ab17c7cb18feb54aca89bb335
Prince Kumar <princesd991@gmail.com>
2024-12-28 16:20:32 +0530

    auth routes update 📦

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,26 +1,37 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
-const multer = require("multer");
+const {
+  register,
+  login,
+  getAllUser,
+  updateUserRole,
+  updateProfileImage,
+} = require("../controllers/authController");
+// const multer = require("multer");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const suffix = Date.now();
-    cb(null, suffix + "-" + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     const suffix = Date.now();
+//     cb(null, suffix + "-" + file.originalname);
+//   },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-router.post("/uploadSingleImage", upload.single("avatar"), (req, res) => {
-  res.send("Single file uploaded successfully");
-});
+// router.post("/uploadSingleImage", upload.single("avatar"), (req, res) => {
+//   res.send("Single file uploaded successfully");
+//   console.log("Here request from body", req.body);
+//   console.log("Here request from file", req.file);
+// });
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/allUsers", getAllUser);
+router.patch("/updateRole", updateUserRole);
+router.patch("/uploadAvatar", updateProfileImage);
 
 module.exports = router;
